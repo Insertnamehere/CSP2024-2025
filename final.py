daccbfbddffeaa133206343c00f6decc343fc586
Insertnamehere <120700189+Insertnamehere@users.noreply.github.com>
2025-04-24 11:18:52 -0700

    Update final.py

--- final.py
+++ final.py
@@ -2,13 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-dataEx = np.loadtxt("stats.txt")
+selectVar = 0
 
-x = dataEx[:,0]
-y = dataEx[:,1]
-zip(x, y)
+while(selectVar == 0):
+    print("Welcome to Data Graphing and Calculations.\nPlease Select:\n0: Menu\n1: .txt file select\n2: Graph Data\n3: ")
+    selectVar = int(input())
 
-x.sort()
+while(selectVar == 1):
+    file = input("Write the full name of your file:\n")
+    if(".txt" not in file):
+        print("Please use file type such as .txt and try again")
+        file = None
+        selectVar = 1
+    else:
+        data = np.loadtxt(file)
+        print("Successfully loaded!")
+    
 
-plt.plot(x, y, '-', color='purple')
-plt.show()
+#dataEx = np.loadtxt()
+
+#x = dataEx[:,0]
+#y = dataEx[:,1]
+#zip(x, y)
+
+#x.sort()
+
+#plt.plot(x, y, '-', color='purple')
+#plt.show()
