319ebcd9ed83536e01da98f07fbcd20a7c076040
Insertnamehere <120700189+Insertnamehere@users.noreply.github.com>
2025-05-30 11:20:56 -0700

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -7,8 +7,15 @@ wn.geometry("1600x1200")
 
 secondsBase, minutesBase, hoursBase = 0, 0, 0
 
-stopCondition = True
+stopCondition = False
 resetShow = False
+
+
+configTime = tk.StringVar(wn)
+
+configSecs = tk.StringVar(wn)
+configMins = tk.StringVar(wn)
+configHours = tk.StringVar(wn)
 
 imgPath = "/Users/andrewkowalski/Documents/APCPS/images.png"
 
@@ -26,43 +33,60 @@ except Exception as e:
 timeLabel = tk.Label(wn, text="00:00:00", font=("Arial", 100,"bold"))
 timeLabel.pack()
 
-def stop():
-    stopCondition = True
-    return stopCondition
-
-def update(stopCondition, seconds, minutes, hours):
-    stopCondition = False
+def update(stopCondition, resetCondition, seconds, minutes, hours):
     if(stopCondition == False):
-        if(seconds > 59):
+        if(seconds >= 59):
             minutes += 1
             seconds = 0
-        elif(minutes > 59):
+        elif(minutes >= 59):
             hours += 1
             minutes = 0
             seconds = 0
-        configSecs = tk.StringVar(value=str(seconds)+":")
-        configMins = tk.StringVar(wn, str(minutes) + ":")
-        configHours = tk.StringVar(wn, str(hours) +":")
-        configTime = tk.StringVar(wn, value=configHours+ configMins+ configSecs)
-        timeLabel.config(text= configTime)
-        print(configTime)
+        else:
+            seconds += 1
+            
+        configSecs.set(value=str(f"{seconds:02d}"))
+
+        configMins.set(value=str(f"{minutes:02d}"))
+
+        configHours.set(value=str(f"{hours:02d}"))
+
+        configTime = tk.StringVar()
+        configTime.set(f"{configHours.get()} {":"} {configMins.get()} {":"} {configSecs.get()}")
         timeLabel.after(1000, lambda:(update(stop(), seconds, minutes, hours)))
-        
+
+        timeLabel.config(text= configTime.get())
+        print(configTime.get())
+
+    if(stopCondition == True):
+        configTime = tk.StringVar()
+        configTime.set(f"{configHours.get()} {configMins.get()} {configSecs.get()}")
+        timeLabel.after(1000, lambda:(update(stop(), seconds, minutes, hours)))
+
+        timeLabel.config(text= configTime.get())
+        print(configTime.get())
+
+    if(resetCondition == True):
+        timeLabel.config(text= ("00:00:00"))
+
     resetShow = True
     return resetShow
 
-def reset(seconds, minutes, hours):
-    seconds, minutes, hours = 0, 0, 0
-    stopCondition = True
-    return stopc, seconds, minutes, hours
+def reset(resetted):
+    resetCondition = resetted
+    return stop(True), resetCondition
+
+def stop(stopped):
+    stopCondition = stopped
+    return stopCondition
 
 
-startButton = tk.Button(wn, text="Start", width=30, height=3, font=("Arial",50,"bold"), command= lambda:(update(stop(), secondsBase, minutesBase, hoursBase), startHide(), resetEnable(), stopShow()))
+startButton = tk.Button(wn, text="Start", width=30, height=3, font=("Arial",50,"bold"), command= lambda:(update(stop(False), reset(False), secondsBase, minutesBase, hoursBase), startHide(), resetEnable(), stopShow()))
 startButton.pack()
 
-stopButton = tk.Button(wn, text="Stop", width=30, height=3, font=("Arial",40,"bold"), command= lambda:(stop(), startShow(), stopHide()))
+stopButton = tk.Button(wn, text="Stop", width=30, height=3, font=("Arial",40,"bold"), command= lambda:(stop(True), startShow(), stopHide()))
 
-resetButton = tk.Button(wn, text="reset", width=30, height=3, font=("Arial",30,"bold"), state= tk.DISABLED, command= lambda:(reset(secondsBase, minutesBase, hoursBase), resetDisable(), startShow(),stopHide()))
+resetButton = tk.Button(wn, text="reset", width=30, height=3, font=("Arial",30,"bold"), state= tk.DISABLED, command= lambda:(reset(True), resetDisable(), startShow(),stopHide()))
 resetButton.pack()
 
 imgLabel.pack()
